2d9b78c746769d73ca9a026a87e54b0cc8a015b2
Felicia Rizka Putri <feliciarizkaputri@gmail.com>
2026-07-03 19:16:15 +0700

    Fix import case for Translator.js

--- morseify/src/components/translator/TranslatorCard.jsx
+++ morseify/src/components/translator/TranslatorCard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { detectAndTranslate } from "../../utils/translator.js";
+import { detectAndTranslate } from "../../utils/Translator.js";
 
 export default function TranslatorCard() {
   const [input, setInput] = useState("");
